d1d8626369d28b8da5c3efca3c5c3a7ee99752d8
Neksha DeSilva <nekshavs@gmail.com>
2025-05-12 09:47:58 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -180,7 +180,11 @@ var htmlJS={
         <div class="signIn-Box_inner signIn-6Box" style="align-items: center;" data-imp="TermsConditions_path">
             <div class="headerText_bool-welcome" style="justify-content: center;">Privacy Policy</div>
             <div class="overlayDiv-tandcviewer_controlCpanel">
-                <b>Copyright (c) 2025 NekshaDeSilva. All rights reserved by Indexx, inc.</b>.<br> All data entered will be encrypted and entirely managed by you in the future. We are not liable for any incidents resulting from your limited interaction or illegal use of data. Account statuses will be displayed approximately two to three weeks after account creation. In such cases, accounts containing illegal or abusive content will be automatically terminated, and all associated data and settings will be permanently erased, even if said accounts are verified on NCloud.<br> Data entered will be encrypted using cryptoAES, ensuring security until exposed to the public.<br>Your Accounts are always free to use, and any actions taken against the legal system of your country will be notified, with appropriate actions taken automatically.<br><b>NCloud PLC is a registered company in Sri Lanka.</b> For assistance, please contact us at hey@nekshavs.tech (temp.).
+               <b>Copyright &copy; 2025 NekshaDeSilva. All rights reserved by Indexx, Inc.</b><br>
+All data entered will be encrypted and managed entirely by you in the future. We are not liable for any incidents resulting from your limited interaction or illegal use of data. Account statuses will be displayed approximately two to three weeks after account creation. Accounts containing illegal or abusive content will be automatically terminated, and all associated data and settings will be permanently erased, even if those accounts are verified on Platform.<br>
+Data entered will be encrypted using cryptoAES, ensuring security until it is exposed to the public.<br>
+Your accounts are always free to use. Any actions taken against the legal system of your country will be notified, and appropriate actions will be taken automatically.<br>
+<b>Indexx, Incorporated is a registered multinational company. This website is for maintenance purposes only and is not affiliated with Indexx, Inc. or any of its subsidiaries.</b>
             </div>
             <div class="keyDiv-accept-decline_i_">
                 <div class="keyDiv-accept-decline__ keyDivFy-dingiN_decline">Help</div>
@@ -210,7 +214,7 @@ var htmlJS={
                         <svg style="margin-right: .2rem;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56"/>
                           </svg>
-                          NCloud Account</div>
+                          NekshaDeSilva</div>
                     <span class="Ugkugsktttt9298">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
